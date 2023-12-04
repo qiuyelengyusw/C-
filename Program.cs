cb9cac1f9396d7ace68203db95f4748973d28a3a
Administrator <Administrator@CHINAMI-GIK6B0C>
2023-12-04 21:09:37 +0800

    添加项目文件。

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hellowork
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //这行代码的作用是将 Hello word 打印输出到控制台中
+            Console.WriteLine("Hello Word!");
+            //此语句的作用是暂停程序
+            Console.ReadLine(); 
+            Console.WriteLine("I Am Hello World Too");
+            Console.ReadLine();
+        }
+    }
+}
