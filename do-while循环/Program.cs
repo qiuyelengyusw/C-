46ebd63c30e38a55d9f0dc960a493e74804eebd8
qiuyelengyusw <qingfeng20231104@gmail.com>
2023-12-16 11:41:14 +0800

    添加注释

--- do-while循环/Program.cs
+++ do-while循环/Program.cs
@@ -13,7 +13,7 @@ namespace do_while循环
             string strAnser;
             do  //do while循环的特点是：先执行一遍语句，然后通过while中的循环条件来判断是否继续执行循环
             {
-                Console.WriteLine("我回答的正确吗？");
+                Console.WriteLine("我回答的正确吗？"); //循环体
                 strAnser = Console.ReadLine();  
             } while (strAnser!="yes"); //循环条件是用户输入的内容不是"yes"的情况下，执行循环体
             Console.ReadKey();
