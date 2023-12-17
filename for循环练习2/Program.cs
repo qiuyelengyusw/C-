6e9127656d8a648b5617a061de53b73968a2b251
qiuyelengyusw <qiuyelengyu@qq.com>
2023-12-17 21:05:15 +0800

    更改优化代码，增加错误检测代码

--- for循环练习2/Program.cs
+++ for循环练习2/Program.cs
@@ -10,10 +10,31 @@ namespace for循环练习2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("请输入一个整数:");
-            string strNumber = Console.ReadLine(); 
-            int intNumber = Convert.ToInt32(strNumber);
-            for (int i = 0; i <=intNumber; i++)
+            int intNumber=0;           
+            string strNumber;           
+            while (true) 
+            {
+                Console.WriteLine("请输入一个数字:");
+                strNumber = Console.ReadLine();
+                if (int.TryParse(strNumber, out intNumber))
+                {
+                    if (Convert.ToInt32(strNumber) > 0)
+                    {
+                        intNumber = Convert.ToInt32(strNumber);
+                        break;                       
+                    }
+                    else
+                    {
+                        Console.WriteLine("输入的数字不能小于或者等于0！");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("输入错误，请重新输入！");
+                   
+                }
+            }
+            for (int i = 0; i <= intNumber; i++)
             {
                 Console.WriteLine("{0} + {1} = {2}",i,intNumber-i,intNumber);
             }
