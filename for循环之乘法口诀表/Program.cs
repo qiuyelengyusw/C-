2f077379e8623f59ed72301bd0bd91632ef30c57
qiuyelengyusw <qiuyelengyu@qq.com>
2023-12-17 17:47:55 +0800

    用for循环的嵌套实现输出乘法口诀表

---
+++ for循环之乘法口诀表/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace for循环之乘法口诀表
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i < 10; i++)
+            {
+
+                for (int j = 1; j<= i; j++)
+                {
+                    Console.Write("{0}*{1}={2}\t",j,i,i*j);                   
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+    }
+}
