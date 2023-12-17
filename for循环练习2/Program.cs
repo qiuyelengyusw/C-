184d6abf6913488abd804ff901dc9b15d6cd7659
qiuyelengyusw <qiuyelengyu@qq.com>
2023-12-17 18:52:29 +0800

    for循环练习2

---
+++ for循环练习2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace for循环练习2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("请输入一个整数:");
+            string strNumber = Console.ReadLine(); 
+            int intNumber = Convert.ToInt32(strNumber);
+            for (int i = 0; i <=intNumber; i++)
+            {
+                Console.WriteLine("{0} + {1} = {2}",i,intNumber-i,intNumber);
+            }
+            Console.ReadKey();
+        }
+    }
+}
