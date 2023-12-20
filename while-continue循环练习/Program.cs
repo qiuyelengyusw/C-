8231585eb3cfae538d80e05dee55fc7c824b13a2
qiuyelengyusw <qiuyelengyu@qq.com>
2023-12-20 17:43:43 +0800

    分别用while循环语句与while contiune循环语句实现1到100之间所有不能被7整除的整数的和

---
+++ while-continue循环练习/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace while_continue循环练习
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int intNumber = 1;
+            int intSum = 0;
+
+            //下面使用while循环实现1到100之间，不能被7整除的所有整数的和
+            while (intNumber <= 100 )
+            {
+                if (intNumber % 7 != 0)
+                {
+                    intSum += intNumber;
+                }               
+                intNumber++;
+            }
+
+            //下面使用while continue循环语句实现上面的功能
+            
+            //while (intNumber <= 100) 
+            //{
+            //    if (intNumber % 7 == 0) 
+            //    {
+            //        intNumber++;
+            //        continue;
+            //    }
+            //    intSum += intNumber;
+            //    intNumber++;
+            //}
+            Console.WriteLine(intSum);
+            Console.ReadKey();
+        }
+    }
+}
