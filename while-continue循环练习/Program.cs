dcd69ee4bf8212470ffb97020c821436e61c8b82
qiuyelengyusw <qiuyelengyu@qq.com>
2023-12-20 18:03:42 +0800

    用for循环语句实现同样的功能

--- while-continue循环练习/Program.cs
+++ while-continue循环练习/Program.cs
@@ -14,14 +14,14 @@ namespace while_continue循环练习
             int intSum = 0;
 
             //下面使用while循环实现1到100之间，不能被7整除的所有整数的和
-            while (intNumber <= 100 )
-            {
-                if (intNumber % 7 != 0)
-                {
-                    intSum += intNumber;
-                }               
-                intNumber++;
-            }
+            //while (intNumber <= 100 )
+            //{
+            //    if (intNumber % 7 != 0)
+            //    {
+            //        intSum += intNumber;
+            //    }               
+            //    intNumber++;
+            //}
 
             //下面使用while continue循环语句实现上面的功能
             
@@ -35,7 +35,18 @@ namespace while_continue循环练习
             //    intSum += intNumber;
             //    intNumber++;
             //}
-            Console.WriteLine(intSum);
+
+
+            //用for 循环语句同样能实现上面的功能
+            for (int i = 0; i < 101; i++)
+            {
+                if (i % 7 != 0) 
+                {
+                    intSum += i;
+
+                }
+            }
+            Console.WriteLine(intSum);            
             Console.ReadKey();
         }
     }
