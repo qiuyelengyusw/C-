f8962d5906a3d28885921e619daf6f21253ceef8
qiuyelengyusw <qiuyelengyu@qq.com>
2023-12-23 16:52:52 +0800

    结构的练习，字段的命名方式关于结构的初步掌握，包括结构的声明方式，以及结构的简单使用方法

---
+++ 结构/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 结构
+{
+    internal class Program
+    {
+        public struct Person //结构的声明方法
+        {
+            public int _age; //public 为了让真个程序都能访问到
+            public string _name;
+            public char _gender;
+        }
+        static void Main(string[] args)
+        {
+            Person zsPerson;
+            Console.WriteLine("Please input the first name:");
+            zsPerson._name = Console.ReadLine();
+            Console.WriteLine("Please input age:");
+            zsPerson._age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please input gender");
+            zsPerson._gender =Convert.ToChar(Console.ReadLine());
+            Console.WriteLine("Name:{0}\nAge:{1}\ngender:{2}", zsPerson._name, zsPerson._age,zsPerson._gender);
+            Console.ReadKey();
+        }
+    }
+}
