67cf790be25b530e406efb4e912b812ca605dda2
qiuyelengyusw <83708132+qiuyelengyusw@users.noreply.github.com>
2023-12-25 12:43:06 +0800

    结构的声明位置，以及格式

--- 结构的学习/Program.cs
+++ 结构的学习/Program.cs
@@ -8,6 +8,12 @@ namespace 结构的学习
 {
     internal class Program
     {
+        public struct Person
+        {
+            public int _age;
+            public string _name;
+            public char _gerden;
+        }
         static void Main(string[] args)
         {
 
