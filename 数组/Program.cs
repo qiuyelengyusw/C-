098d4b8d06f50d32770bf2346bc85521d9ff6a83
qiuyelengyusw <83708132+qiuyelengyusw@users.noreply.github.com>
2023-12-25 13:24:15 +0800

    数组的声明方式，以及数组的赋值方法

---
+++ 数组/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 数组
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = new int[10]; //这行代码声明了一个长度是10的整数类型的数组
+            for (int i = 0; i < nums.Length; i++) 
+            {
+                nums[i] = i;
+                Console.WriteLine(nums[i]); 
+            }
+            Console.ReadKey();
+        }
+    }
+}
