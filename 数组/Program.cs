7bf0e9be9754c579e7f38b4700734426c517a12c
qiuyelengyusw <83708132+qiuyelengyusw@users.noreply.github.com>
2023-12-25 14:03:15 +0800

    罪行更改

--- 数组/Program.cs
+++ 数组/Program.cs
@@ -11,10 +11,17 @@ namespace 数组
         static void Main(string[] args)
         {
             int[] nums = new int[10]; //这行代码声明了一个长度是10的整数类型的数组
-            for (int i = 0; i < nums.Length; i++) 
+            for (int i = 0; i < nums.Length; i++)
             {
                 nums[i] = i;
-                Console.WriteLine(nums[i]); 
+                Console.WriteLine(nums[i]);
+            }
+            Console.WriteLine();
+            //数组的第二种声明方法
+            int[] numsTwo = new int[5] { 1, 2, 3, 4, 5 };
+            for (int j = numsTwo.Length - 1; j >= 0; j--)
+            {
+                Console.WriteLine(numsTwo[j]);  
             }
             Console.ReadKey();
         }
