d938b7a1ef9242eff49dd87907834aeb933cfe19
qiuyelengyusw <83708132+qiuyelengyusw@users.noreply.github.com>
2023-12-25 21:16:29 +0800

    冒泡排序的其他方法，Array.Sort(),与Array.Revere()的结合使用可以降序排列数组

--- 冒泡排序/Program.cs
+++ 冒泡排序/Program.cs
@@ -12,6 +12,18 @@ namespace 冒泡排序
         {
             //冒泡排序就是将一个数组中的元素按照从小到大或者从大到小的顺序排列
             int[] ints = { 3, 2, 5, 1, 0, 8, 6, 9, 11, 15, 18, 20, 10, 7, 13, 12, 16, 19, 22, 21, 17, 14, 4, 23, 26, 25, 30, 24, 28 };
+            int[] ints1 = { 3, 2, 5, 1, 0, 8, 6, 9, 11, 15, 18, 20, 10, 7, 13, 12, 16, 19, 22, 21, 17, 14, 4, 23, 26, 25, 30, 24, 28, 27, 50 };
+            #region 方法一：使用自带函数快速排序
+            //
+            Array.Sort(ints1);  //Array.Sort() 只能对数组进行升序排列
+            Array.Reverse(ints1); //翻转数组，配合上面的Sort，可实现数组的降序排列
+
+            for (int i = 0; i < ints1.Length; i++)
+            {
+                Console.WriteLine(ints1[i] + " ");
+            }
+            #endregion
+            #region 方法二：使用for循环嵌套的方法
             for (int i = 0; i < ints.Length - 1; i++)
             {
                 for (int j = 0; j < ints.Length - 1 - i; j++)
@@ -30,6 +42,7 @@ namespace 冒泡排序
             {
                 Console.WriteLine(ints[i] + " ");
             }
+            #endregion
             Console.ReadKey();
         }
     }
