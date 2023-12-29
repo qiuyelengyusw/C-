fcd43dcf8f2d12debb936676c4e6b02f471dfdfc
qiuyelengyusw <83708132+qiuyelengyusw@users.noreply.github.com>
2023-12-29 19:47:56 +0800

    优化代码，添加注释

--- 方法练习之数组/Program.cs
+++ 方法练习之数组/Program.cs
@@ -12,11 +12,11 @@ namespace 方法练习之数组
         static void Main(string[] args)
         {
             int[] ints = { 1, 2, 3, 4, 5, 6, 7, 8, -9, 0,15};
-            int[] inta = Getnumbers(ints);
-            for (int i = 0; i < inta.Length; i++)
-            {
-                Console.WriteLine(inta[i]);
-            }
+            int[] inta = Getnumbers(ints);  //定义一个数组，接受
+            Console.WriteLine("这个数组中最大值是：{0}", inta[0]);
+            Console.WriteLine("这个数组中最大值是：{0}", inta[1]);
+            Console.WriteLine("这个数组中所有元素的总和是：{0}", inta[2]);
+            Console.WriteLine("这个数组的平均值是：{0}", inta[3]);
             Console.ReadKey();
         }
         /// <summary>
@@ -26,29 +26,24 @@ namespace 方法练习之数组
         /// <returns>返回计算的值</returns>
         public static int[] Getnumbers(int[] nums)
         {
-            int[] res = new int[4];
-            int max = nums[0];
-            int min = nums[0];
-            int sum = 0;
-            int average;
+            int[] res = new int[4];    //定义一个数组来存储用户给定的数组中计算出来的最大值、最小值、总和与平均值
+            res[0] = nums[0];   //max      
+            res[1] = nums[0];   //min
+            res[2] = 0;         //sum 
             for (int i = 0; i < nums.Length; i++)
             {
-                if (nums[i] > max)
+                if (nums[i] > res[0])
                 {
-                    max = nums[i];
+                    res[0] = nums[i];  //最大值
                 }
-                if (nums[i] < min)
+                if (nums[i] < res[1])
                 {
-                    min = nums[i];
+                    res[1] = nums[i];  //最小值
                 }
-                sum += nums[i];
+                res[2] += nums[i];   //总和
             }
-            average = sum / nums.Length;
-            res[0] = max;
-            res[1] = min;
-            res[2] = sum;
-            res[3] = average;
-            return res;
-        }
+            res[3] = res[2] / nums.Length;       //平均值     
+            return res; //返回数组
+        }        
     }
 }
