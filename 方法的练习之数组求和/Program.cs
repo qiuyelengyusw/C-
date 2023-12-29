513670b4bb623d5db996b9b2ab7e3a6506683d25
qiuyelengyusw <qingfeng20231104@gmail.com>
2023-12-29 13:28:47 +0800

    方法的练习，限定用户输入内容和数组求和

---
+++ 方法的练习之数组求和/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 方法的练习之数组求和
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            int intSum  = GetSum(nums);
+            Console.WriteLine(intSum);
+            Console.ReadKey();
+        }
+        /// <summary>
+        /// 求一个整数数组的总和
+        /// </summary>
+        /// <param name="numbers">需要求和的数组</param>
+        /// <returns>返回数组的总和</returns>
+        public static int GetSum(int[] numbers)
+        {
+            int sum = 0;
+            for (int i = 0; i < numbers.Length; i++) 
+            {
+                sum += numbers[i];
+            }
+            return sum;
+        }
+    }
+}
---
+++ 用方法计算数组的和/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 用方法计算数组的和
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
