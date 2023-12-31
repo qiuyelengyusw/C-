f5d18cc8af8bc46ea28eae3c63047d1ac43f801e
qiuyelengyusw <83708132+qiuyelengyusw@users.noreply.github.com>
2023-12-31 21:46:56 +0800

    格式化代码

--- 方法的综合练习五/Program.cs
+++ 方法的综合练习五/Program.cs
@@ -21,17 +21,17 @@ namespace 方法的综合练习五
         /// <param name="s">字符串数组</param>
         public static void GetFZ(string[] s)
         {
-            for (int i = 0; i < s.Length/2; i++) 
+            for (int i = 0; i < s.Length / 2; i++)
             {
                 string tep = s[i];
-                s[i] = s[s.Length-1-i];
-                s[s.Length-1-i] = tep;
+                s[i] = s[s.Length - 1 - i];
+                s[s.Length - 1 - i] = tep;
             }
             for (int i = 0; i < s.Length; i++)
             {
-                Console.Write(s[i]+" ");
+                Console.Write(s[i] + " ");
             }
         }
-               
+
     }
 }
