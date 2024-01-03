2157be8ff6245e4de98552644764de31878844d6
qiuyelengyusw <83708132+qiuyelengyusw@users.noreply.github.com>
2024-01-03 21:23:27 +0800

    面向对象，一个简单类的示例

---
+++ 面向对象/Person.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 面向对象
+{
+    internal class Person
+    {
+        public string _name;
+        public int _age;
+        public char _gender;
+
+        public void SQ()
+        {
+            Console.WriteLine("我的名字叫{0},我今年{1}岁，我是{2}生",this._name,this._age,this._gender);
+        }
+
+    }
+}
---
+++ 面向对象/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 面向对象
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person pername = new Person();
+            pername._name = "张三";
+            pername._age = 19;
+            pername._gender = '男';
+            pername.SQ();
+            Console.ReadKey();
+        }
+    }
+}
