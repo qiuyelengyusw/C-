f1b9f2617cb6a8cee0567b842095bf66bc6c82f3
qiuyelengyusw <qingfeng20231104@gmail.com>
2024-01-08 15:30:30 +0800

    删除空格

--- 面向对象/Person.cs
+++ 面向对象/Person.cs
@@ -12,10 +12,9 @@ namespace 面向对象
         public string _name;
         public int _age;
         public char _gender;
-
         public void SQ()
         {
-            Console.WriteLine("我的名字叫{0},我今年{1}岁，我是{2}生",this._name,this._age,this._gender);
+            Console.WriteLine("我的名字叫{0},我今年{1}岁，我是{2}生", this._name, this._age, this._gender);
         }
 
     }
