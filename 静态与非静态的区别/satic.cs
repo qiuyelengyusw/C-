75ada6c630f65b5f6fda52b532eb37f02c26bbee
qiuyelengyusw <qingfeng20231104@gmail.com>
2024-01-11 16:10:32 +0800

    1

--- 静态与非静态的区别/satic.cs
+++ 静态与非静态的区别/satic.cs
@@ -1,13 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace 静态与非静态的区别
 {
-    internal class satic
+    public class satic
     {
+        private static string _name;
+        public static string name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+        private int _age;
         public void M1()
         {
             Console.WriteLine("我是非静态的方法");
