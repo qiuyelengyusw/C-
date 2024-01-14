de28da3a4669e1cc2590e794ddc1123948d57aed
qiuyelengyusw <83708132+qiuyelengyusw@users.noreply.github.com>
2024-01-14 16:54:38 +0800

    判断字符串中是否有指定的字符，将指定的字符替换成其他字符

---
+++ 字符串二/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 字符串二
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string strA = "国家关键人物老王";
+            if(strA.Contains("老王")) //判断字符串中是否存在指定的字符
+            {
+               strA= strA.Replace("老王","**");  //将字符串中的字符替换成其他字符
+            }
+            Console.WriteLine(strA);
+            Console.ReadKey();
+        }
+    }
+}
