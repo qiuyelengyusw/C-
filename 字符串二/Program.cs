e0aae1bc4e300231d51bf196d7cb7c0eb3348143
qiuyelengyusw <qingfeng20231104@gmail.com>
2024-01-15 13:52:04 +0800

    格式化代码

--- 字符串二/Program.cs
+++ 字符串二/Program.cs
@@ -11,15 +11,15 @@ namespace 字符串二
         static void Main(string[] args)
         {
             string strA = "国家关键人物老王";
-            if(strA.Contains("老王")) //判断字符串中是否存在指定的字符
+            if (strA.Contains("老王")) //判断字符串中是否存在指定的字符
             {
-               strA= strA.Replace("老王","**");  //将字符串中的字符替换成其他字符
+                strA = strA.Replace("老王", "**");  //将字符串中的字符替换成其他字符
             }
             Console.WriteLine(strA);
             Console.ReadKey();
             //Substring 截取字符串
             string strB = "把所有想的念的错的痛的都放在心房，行李箱里装不下我想要去的远方";
-            if(strB.StartsWith("把所有")&&strB.EndsWith("远方"))
+            if (strB.StartsWith("把所有") && strB.EndsWith("远方"))
             {
                 Console.WriteLine("是的");
             }
@@ -28,11 +28,11 @@ namespace 字符串二
                 Console.WriteLine("不是的");
             }
             //indexof()
-            int index = strB.IndexOf('心',0); //indexof(",搜索的起始位置)
-            Console.WriteLine("心"+"第一次出现的地方是{0}",index);
-            strB = strB.Substring(10,4);
-            Console.WriteLine(strB);    
-            Console.ReadKey();          
+            int index = strB.IndexOf('心', 0); //indexof(",搜索的起始位置)
+            Console.WriteLine("心" + "第一次出现的地方是{0}", index);
+            strB = strB.Substring(10, 4);
+            Console.WriteLine(strB);
+            Console.ReadKey();
 
         }
     }
