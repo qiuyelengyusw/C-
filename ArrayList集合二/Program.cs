078a8eff9ab57be06c58b4c3e3299af8f5223a39
qiuyelengyusw <qingfeng20231104@gmail.com>
2024-01-18 16:19:24 +0800

    向ArrayList集合中添加数组的简洁方法

---
+++ ArrayList集合二/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+namespace ArrayList集合二
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList list = new ArrayList();
+            list.Add(1);
+            list.Add(true);
+            list.Add("张三");
+            list.AddRange(new string[] { "李四", "王五", "赵六" });
+            list.AddRange(new int[] { 1, 2, 3, 4, 5, 6 });
+            for (int i = 0; i < list.Count; i++)
+            {
+                Console.WriteLine(list[i]);
+            }
+            Console.ReadKey();
+        }
+    }
+}
