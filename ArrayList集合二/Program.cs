7269cbd231e594f0824604bf6ab046584777b334
qiuyelengyusw <qingfeng20231104@gmail.com>
2024-01-18 16:32:33 +0800

    添加集合方法，以及清除集合内所有元素的方法，clear（）

--- ArrayList集合二/Program.cs
+++ ArrayList集合二/Program.cs
@@ -14,8 +14,10 @@ namespace ArrayList集合二
             list.Add(1);
             list.Add(true);
             list.Add("张三");
+            //添加集合元素
             list.AddRange(new string[] { "李四", "王五", "赵六" });
             list.AddRange(new int[] { 1, 2, 3, 4, 5, 6 });
+            list.AddRange(list); //添加集合元素
             for (int i = 0; i < list.Count; i++)
             {
                 Console.WriteLine(list[i]);
