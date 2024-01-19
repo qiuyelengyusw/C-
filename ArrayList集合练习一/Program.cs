456667d5aebfabede11bb96a41934d80e34335e7
qiuyelengyusw <qingfeng20231104@gmail.com>
2024-01-20 07:47:04 +0800

    Array List方法

--- ArrayList集合练习一/Program.cs
+++ ArrayList集合练习一/Program.cs
@@ -13,12 +13,14 @@ namespace ArrayList集合练习一
         {
             //创建一个集合，里面添加一些数字，求平均值与总和
             ArrayList list = new ArrayList();
-            list.AddRange(new double[] { 1, 2, 3.14, 4.25, 5, 6.36, 7, 8, 9.14 });
+            list.AddRange(new double[] { 1, 2, 3.14, 4.25, 5, 6.36, 7, 8, 9.14, 2.36 });
             double dblSum = 0;
-            for (int i = 0; i < list.Count; i++) 
+            for (int i = 0; i < list.Count; i++)
             {
-                dblSum += (int)list[i];
+                dblSum += (double)list[i];
             }
+            Console.WriteLine("这个集合内数字的总和是{0},平均值是{1}", dblSum, Math.Round(dblSum / list.Count, 2));
+            Console.ReadKey();
         }
     }
 }
