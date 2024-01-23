2473d98ddee1809b0e2578fbe39954b1d976feb8
qiuyelengyusw <qingfeng20231104@gmail.com>
2024-01-23 14:54:40 +0800

    foreach()循环的基本用法

---
+++ foreach循环/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace foreach循环
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] ints = { 1, 2, 3, 5, 6, 7, 8, 9 };
+            for (int i = 0; i < ints.Length; i++) 
+            {
+                Console.WriteLine(ints[i]);
+            }
+            Console.ReadKey();
+            Console.WriteLine("<><><><><><><><><><><><><><><><><>");
+            foreach ( int i in ints ) 
+            {
+                Console.WriteLine(i);
+            }
+            Console.ReadKey();
+        }
+    }
+}
