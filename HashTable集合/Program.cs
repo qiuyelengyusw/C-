9647b8f8c795dcb6c9089f24d80414a3aa7b469f
qiuyelengyusw <qingfeng20231104@gmail.com>
2024-01-23 14:37:30 +0800

    HashTable集合 ，foreach()循环方法

---
+++ HashTable集合/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashTable集合
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Hashtable ht = new Hashtable(); //创建键值对集合对象
+            ht.Add(1, 1);
+            ht.Add(2, "张三");
+            ht.Add(3, true);
+            ht.Add(4, 3.14);
+            ht.Add(5, '男');
+            ht.Add(false, "错误");
+            foreach (var item in ht.Keys)
+            {
+                Console.WriteLine(ht[item]);
+            }
+            Console.ReadKey();
+        }
+    }
+}
