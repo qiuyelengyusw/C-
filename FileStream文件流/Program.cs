314239b7364252fd6bcb7321ee79df54c41957e8
qiuyelengyusw <83708132+qiuyelengyusw@users.noreply.github.com>
2024-01-24 20:57:24 +0800

    使用FileStream实现多媒体文件的复制

--- FileStream文件流/Program.cs
+++ FileStream文件流/Program.cs
@@ -23,14 +23,50 @@ namespace FileStream文件流
             Console.WriteLine(s);
             Console.ReadKey();
             //使用FileStream来写入数据
-            using (FileStream fswrite = new FileStream(@"F:\session1.log",FileMode.OpenOrCreate,FileAccess.Write))
+            using (FileStream fswrite = new FileStream(@"F:\session1.log", FileMode.OpenOrCreate, FileAccess.Write)) //使用using()目的是让程序自动释放流占用的资源，可以省略.close()和.Dispose()语句
             {
                 string str = "这是写入的数据";
-                byte[] buffer1=Encoding.Default.GetBytes(str);
+                byte[] buffer1 = Encoding.Default.GetBytes(str);
                 fswrite.Write(buffer1, 0, buffer1.Length);
-                Console.WriteLine("数据已写入");
-                Console.ReadKey();
             }
+            Console.WriteLine("数据已写入");
+            Console.ReadKey();
+            //使用FileStream实现多媒体文件的复制
+            string source = @"D:\音乐\蒙娜丽莎的眼泪.ape";
+            string target = @"F:\蒙娜丽莎的眼泪new.ape";
+            CopyFile(source, target);
+            Console.WriteLine("复制成功");
+            Console.ReadKey();
+        }
+        /// <summary>
+        /// 读取和写入的方法
+        /// </summary>
+        /// <param name="source"></param>
+        /// <param name="target"></param>
+        public static void CopyFile(string source, string target) 
+        {
+            //创建一个负责读取的流
+            using (FileStream fsread = new FileStream(source, FileMode.OpenOrCreate, FileAccess.Read))
+            {
+                using (FileStream fswrite = new FileStream(target, FileMode.OpenOrCreate, FileAccess.Write))
+                {
+                    byte[] buffer = new byte[1024 * 1024 * 5];
+                    //文件可能会比较大，需要通过循环读取
+                    while (true)
+                    {
+                        //返回本次实际读取到的字节数
+                        int r = fsread.Read(buffer, 0, buffer.Length);
+                        //如果r返回0，则说明读取完毕
+                        if (r == 0)
+                        {
+                            break;
+                        }
+                        fswrite.Write(buffer, 0, r);
+                    }
+                }
+
+            }
+
         }
     }
 }
