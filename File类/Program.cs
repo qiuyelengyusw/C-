ebc250f11c950922f03e6cafe5a9d58dfa7bc31b
qiuyelengyusw <qingfeng20231104@gmail.com>
2024-01-24 10:00:06 +0800

    Path类及File类的用法

---
+++ File类/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace File类
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //创建一个新文件
+            //File.Create(@"E:\new.txt");
+            //Console.WriteLine("创建成功");
+            //Console.ReadKey();
+            //删除文件
+            //File.Delete(@"E:\new.txt");
+            //Console.WriteLine("删除成功");
+            //Console.ReadKey();
+            //复制文件
+            //File.Copy(@"E:\new.txt", @"E:\new1.txt");
+            //Console.WriteLine("复制文件成功");
+            //Console.ReadKey();
+
+        }
+    }
+}
---
+++ Path类/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Path类
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = @"D:\生威的腾讯微云\长城电脑\08-UOS系统间文件共享指南.doc";
+            //获得文件名和扩展名            
+            Console.WriteLine(Path.GetFileName(str));
+            //获得文件的扩展名
+            Console.WriteLine(Path.GetExtension(str));
+            //获得文件名，不包含扩展名
+            Console.WriteLine(Path.GetFileNameWithoutExtension(str));
+            //获得文件的路径
+            Console.WriteLine(Path.GetDirectoryName(str));
+            //获得文件的绝对路径
+            Console.WriteLine(Path.GetFullPath(str));
+            //将路径连接到一起
+            Console.WriteLine(Path.Combine(@"d:\a\","b.txt"));
+            Console.ReadKey();
+        }
+    }
+}
