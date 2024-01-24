134dddbc606b6caabdab3ebe5fea0cf64c942826
qiuyelengyusw <qingfeng20231104@gmail.com>
2024-01-24 11:05:51 +0800

    File类的读写方法

--- File类/Program.cs
+++ File类/Program.cs
@@ -23,7 +23,34 @@ namespace File类
             //File.Copy(@"E:\new.txt", @"E:\new1.txt");
             //Console.WriteLine("复制文件成功");
             //Console.ReadKey();
-
+            //将内容写进文本文件中
+            string str = "把所有想的念的错的痛的都放在心脏，行李箱里装不下我想去的远方。";
+            byte[] buffer = Encoding.Default.GetBytes(str);
+            File.WriteAllBytes(@"E:\new.txt", buffer);
+            Console.WriteLine("写入成功");
+            Console.ReadKey();
+            //已行的形式读出文件中的内容
+            string[] stra = File.ReadAllLines(@"E:\new.txt", Encoding.Default);
+            foreach (string str2 in stra)
+            {
+                Console.WriteLine(str2);
+            }
+            Console.ReadKey();
+            string strb = File.ReadAllText(@"E:\new.txt", Encoding.Default);
+            Console.WriteLine(strb);
+            Console.ReadKey();
+            File.WriteAllLines(@"E:\new1.txt", new string[] { "从来都没有", "一个人像你爱我这么久" });
+            Console.WriteLine("写入成功");
+            Console.ReadKey();
+            File.WriteAllText(@"E:\new.txt", "这个是新写入的内容", Encoding.Default);
+            Console.WriteLine("重新写入成功");
+            Console.ReadKey();
+            File.AppendAllLines(@"E:\new.txt", new string[] { "\nAppendAllLined追加的文件"},Encoding.Default);
+            Console.WriteLine("追加内容成功");
+            Console.ReadKey();
+            File.AppendAllText(@"E:\new.txt","AppendAllText追加的内容",Encoding.Default);
+            Console.WriteLine("追加内容成功");
+            Console.ReadKey();            
         }
     }
 }
