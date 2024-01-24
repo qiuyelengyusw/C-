f0d75d466c5dda93e6d22b19b5ec121bcd98b2d0
qiuyelengyusw <qingfeng20231104@gmail.com>
2024-01-24 13:33:16 +0800

    icitionary泛型集合

---
+++ Dicitionary泛型集合/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dicitionary泛型集合
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int, string> dic = new Dictionary<int, string>();
+            dic.Add(1, "张三");
+            dic.Add(2, "李四");
+            dic.Add(3, "王五");
+            dic[4] = "赵六";
+            foreach (var item in dic.Keys)
+            {
+                Console.Write(dic[item] + " ");
+            }
+            Console.ReadKey();
+            Console.WriteLine();
+            foreach (KeyValuePair<int, string> kv in dic)
+            {
+                Console.WriteLine(kv.Key + ">>>>>>>>>>>" + kv.Value);
+            }
+            Console.ReadLine();
+        }
+    }
+}
