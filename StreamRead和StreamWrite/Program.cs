ee26190d4da3c9fe3eedd0394cec40618c9ca667
qiuyelengyusw <83708132+qiuyelengyusw@users.noreply.github.com>
2024-01-25 20:38:37 +0800

    StreamRead和StreamWrite的基本使用方法

---
+++ StreamRead和StreamWrite/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StreamRead和StreamWrite
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using (StreamReader sr=new StreamReader(@"D:\星际争霸秘籍.txt"))
+            {
+                Console.WriteLine(sr.ReadToEnd());
+            }
+            Console.ReadKey();
+            using (StreamReader sr1 = new StreamReader(@"D:\星际争霸秘籍.txt"))
+            {
+                while (!sr1.EndOfStream)
+                {
+                    Console.WriteLine(sr1.ReadLine());
+                }
+                Console.ReadKey();
+            }
+            using (StreamWriter sw = new StreamWriter(@"D:\星际争霸秘籍new.txt"))
+            {
+                sw.WriteLine("这是一个新的文件", true);
+            }
+            Console.ReadKey();
+        }
+    }
+}
