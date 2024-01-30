51772cee765e099696619e621b1733cffc1a4aaf
qiuyelengyusw <83708132+qiuyelengyusw@users.noreply.github.com>
2024-01-30 22:19:59 +0800

    winform的简单应用

---
+++ 移动按钮/Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace 移动按钮
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.butA = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // butA
+            // 
+            this.butA.Location = new System.Drawing.Point(285, 175);
+            this.butA.Name = "butA";
+            this.butA.Size = new System.Drawing.Size(147, 50);
+            this.butA.TabIndex = 0;
+            this.butA.Text = "点不到";
+            this.butA.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.butA);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.MouseEnter += new System.EventHandler(this.Form1_MouseEnter);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button butA;
+    }
+}
+
---
+++ 移动按钮/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace 移动按钮
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_MouseEnter(object sender, EventArgs e)
+        {
+            int dblx=this.ClientSize.Width-butA.ClientSize.Width; //按钮移动的最大宽度是工作区的宽度减去按钮的宽度
+            int dbly=this.ClientSize.Height-butA.ClientSize.Height; //按钮移动的最大高度是工作区的高度减去按钮的高度
+            Random random = new Random();
+            butA.Location = new Point(random.Next(0,dblx+1),random.Next(0,dbly+1)); //给按钮一个新的坐标
+        }
+    }
+}
