606d7a8b20b89ac1af9c5ebe46f0d312b4ad3832
qiuyelengyusw <qingfeng20231104@gmail.com>
2024-01-31 15:48:09 +0800

    winform联系之跑马灯

---
+++ Winform应用/Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace Winform应用
+{
+    partial class frma
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.lbla = new System.Windows.Forms.Label();
+            this.tima = new System.Windows.Forms.Timer(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lbla
+            // 
+            this.lbla.Font = new System.Drawing.Font("宋体", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.lbla.Location = new System.Drawing.Point(237, 189);
+            this.lbla.Name = "lbla";
+            this.lbla.Size = new System.Drawing.Size(310, 36);
+            this.lbla.TabIndex = 0;
+            // 
+            // tima
+            // 
+            this.tima.Enabled = true;
+            this.tima.Tick += new System.EventHandler(this.tima_Tick);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(241, 352);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "开始";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(471, 352);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "结束";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // frma
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.lbla);
+            this.Name = "frma";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbla;
+        private System.Windows.Forms.Timer tima;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Winform应用/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Winform应用
+{
+    public partial class frma : Form
+    {
+        public frma()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.tima.Stop();  //再窗体初始化得时候，让timer停止
+        }
+        string stra = "✩✬✭✩✬✩✬✭✩✬✩✬✭✩✬✩✬✭✩✬"; //定义一个字符串
+        private void tima_Tick(object sender, EventArgs e)
+        {           
+            stra = stra.Substring(1) + stra.Substring(0, 1);    //将字符串从第二个元素截取，然后将第一个元素连接       
+            lbla.Text = stra; //将字符串赋值给lbl
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.tima.Start(); //开始
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.tima.Stop();//停止
+        }
+    }
+}
