aa4011ff7b85bcb1472a905ba151a39e8e227eb3
Administrator <qiuyelengyu@qq.com>
2025-03-10 17:25:19 +0800

    无

--- 冒泡排序二/Program.cs
+++ 冒泡排序二/Program.cs
@@ -11,7 +11,7 @@ namespace 冒泡排序二
         static void Main(string[] args)
         {
             int[] ints1 = { 3, 2, 5, 1, 0, 8, 6, 9, 11, 15, 18, 20, 10, 7, 13, 12, 16, 19, 22, 21, 17, 14, 4, 23, 26, 25, 30, 24, 28, 27, 50 }; //定义一个数组
-            for (int i = 0; i < ints1.Length +1; i++) //外层循环控制排序趟数
+            for (int i = 0; i < ints1.Length + 1; i++) //外层循环控制排序趟数
             {
                 for (int j = 0; j < ints1.Length - 1 - i; j++) //内层循环控制每一趟排序多少次
                 {
