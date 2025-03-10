96fd3db35ff5cdb15567bf0bab6e6e54cc4ba493
Administrator <qiuyelengyu@qq.com>
2025-03-10 12:41:09 +0800

    列表

---
+++ 列表/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 列表
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var names = new List<string> { "<name>", "Maria", "Jerry" }; // 创建一个列表
+            foreach (var name in names) // 遍历列表
+            {
+                Console.WriteLine($"Hello {name.ToUpper()}!");
+            }
+            Console.ReadKey();
+        }
+    }
+}
