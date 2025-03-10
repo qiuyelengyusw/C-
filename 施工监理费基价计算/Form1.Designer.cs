c3303023dc3a295b9e8c37f051bf4d765dc70a69
Administrator <qiuyelengyu@qq.com>
2025-03-10 22:21:59 +0800

    制作安装程序

--- 施工监理费基价计算/Form1.Designer.cs
+++ 施工监理费基价计算/Form1.Designer.cs
@@ -55,13 +55,14 @@
             // 
             // txtGczj
             // 
-            this.txtGczj.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtGczj.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.txtGczj.Font = new System.Drawing.Font("微软雅黑", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
             this.txtGczj.ForeColor = System.Drawing.SystemColors.Highlight;
-            this.txtGczj.Location = new System.Drawing.Point(464, 60);
+            this.txtGczj.Location = new System.Drawing.Point(465, 60);
+            this.txtGczj.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.txtGczj.Multiline = true;
             this.txtGczj.Name = "txtGczj";
-            this.txtGczj.Size = new System.Drawing.Size(247, 46);
+            this.txtGczj.Size = new System.Drawing.Size(247, 45);
             this.txtGczj.TabIndex = 0;
             this.txtGczj.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             this.txtGczj.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtGczj_KeyPress);
@@ -71,7 +72,7 @@
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("微软雅黑", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
             this.label2.ForeColor = System.Drawing.SystemColors.MenuHighlight;
-            this.label2.Location = new System.Drawing.Point(94, 153);
+            this.label2.Location = new System.Drawing.Point(93, 153);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(345, 46);
             this.label2.TabIndex = 0;
@@ -79,14 +80,15 @@
             // 
             // txtJlf
             // 
-            this.txtJlf.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtJlf.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.txtJlf.Font = new System.Drawing.Font("微软雅黑", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
             this.txtJlf.ForeColor = System.Drawing.SystemColors.MenuHighlight;
-            this.txtJlf.Location = new System.Drawing.Point(464, 154);
+            this.txtJlf.Location = new System.Drawing.Point(465, 154);
+            this.txtJlf.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.txtJlf.Multiline = true;
             this.txtJlf.Name = "txtJlf";
             this.txtJlf.ReadOnly = true;
-            this.txtJlf.Size = new System.Drawing.Size(247, 46);
+            this.txtJlf.Size = new System.Drawing.Size(247, 45);
             this.txtJlf.TabIndex = 1;
             this.txtJlf.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
@@ -95,9 +97,10 @@
             this.button1.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
             this.button1.Font = new System.Drawing.Font("微软雅黑", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
             this.button1.ForeColor = System.Drawing.SystemColors.Highlight;
-            this.button1.Location = new System.Drawing.Point(32, 423);
+            this.button1.Location = new System.Drawing.Point(33, 423);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(410, 108);
+            this.button1.Size = new System.Drawing.Size(411, 108);
             this.button1.TabIndex = 2;
             this.button1.Text = "折  前   计   算";
             this.button1.UseVisualStyleBackColor = false;
@@ -109,10 +112,11 @@
             this.butclear.Font = new System.Drawing.Font("微软雅黑", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
             this.butclear.ForeColor = System.Drawing.Color.Red;
             this.butclear.Location = new System.Drawing.Point(757, 60);
+            this.butclear.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.butclear.Name = "butclear";
-            this.butclear.Size = new System.Drawing.Size(124, 320);
+            this.butclear.Size = new System.Drawing.Size(123, 320);
             this.butclear.TabIndex = 3;
-            this.butclear.Text = "清 除";
+            this.butclear.Text = "清\r\n\r\n除";
             this.butclear.UseVisualStyleBackColor = false;
             this.butclear.Click += new System.EventHandler(this.butclear_Click);
             // 
@@ -129,14 +133,15 @@
             // 
             // txtZKl
             // 
-            this.txtZKl.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtZKl.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.txtZKl.Font = new System.Drawing.Font("微软雅黑", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
             this.txtZKl.ForeColor = System.Drawing.SystemColors.Highlight;
-            this.txtZKl.Location = new System.Drawing.Point(464, 242);
+            this.txtZKl.Location = new System.Drawing.Point(465, 242);
+            this.txtZKl.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.txtZKl.MaxLength = 4;
             this.txtZKl.Multiline = true;
             this.txtZKl.Name = "txtZKl";
-            this.txtZKl.Size = new System.Drawing.Size(247, 46);
+            this.txtZKl.Size = new System.Drawing.Size(247, 45);
             this.txtZKl.TabIndex = 2;
             this.txtZKl.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             this.txtZKl.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
@@ -147,7 +152,7 @@
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("微软雅黑", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
             this.label4.ForeColor = System.Drawing.SystemColors.MenuHighlight;
-            this.label4.Location = new System.Drawing.Point(24, 333);
+            this.label4.Location = new System.Drawing.Point(24, 334);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(415, 46);
             this.label4.TabIndex = 0;
@@ -156,14 +161,15 @@
             // 
             // txtZK
             // 
-            this.txtZK.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtZK.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.txtZK.Font = new System.Drawing.Font("微软雅黑", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
             this.txtZK.ForeColor = System.Drawing.SystemColors.MenuHighlight;
-            this.txtZK.Location = new System.Drawing.Point(464, 334);
+            this.txtZK.Location = new System.Drawing.Point(465, 334);
+            this.txtZK.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.txtZK.Multiline = true;
             this.txtZK.Name = "txtZK";
             this.txtZK.ReadOnly = true;
-            this.txtZK.Size = new System.Drawing.Size(247, 46);
+            this.txtZK.Size = new System.Drawing.Size(247, 45);
             this.txtZK.TabIndex = 1;
             this.txtZK.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
@@ -173,8 +179,9 @@
             this.button2.Font = new System.Drawing.Font("微软雅黑", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
             this.button2.ForeColor = System.Drawing.SystemColors.Highlight;
             this.button2.Location = new System.Drawing.Point(471, 423);
+            this.button2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(410, 108);
+            this.button2.Size = new System.Drawing.Size(411, 108);
             this.button2.TabIndex = 2;
             this.button2.Text = "折   扣   计   算";
             this.button2.UseVisualStyleBackColor = false;
@@ -184,7 +191,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.LightGoldenrodYellow;
+            this.BackColor = System.Drawing.Color.LightYellow;
             this.ClientSize = new System.Drawing.Size(918, 564);
             this.Controls.Add(this.butclear);
             this.Controls.Add(this.button2);
@@ -199,6 +206,7 @@
             this.Controls.Add(this.label1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "Frm1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "施工监理费基价计算";
