d9474f078c4246041e16ba469cfabb1af63e6ae5
Administrator <qiuyelengyu@qq.com>
2025-03-11 21:27:28 +0800

    数组声明及使用

---
+++ 数组复习/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 数组复习
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //数组类型[] 数组名 = new 数组类型[数组长度];
+            int[] numbers = new int[10];
+            int[] numbers2 = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20 };
+            int[] numbers3 = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            numbers[0] = 1;
+            numbers[1] = 2;
+            numbers[2] = 3;
+            numbers[3] = 4;
+            numbers[4] = 5;
+            numbers[5] = 6;
+            numbers[6] = 7;
+            numbers[7] = 8;
+            numbers[8] = 9;
+            numbers[9] = 10;
+            foreach (int  num in numbers2)
+            {
+                Console.Write(num + " ");
+            }
+            Console.WriteLine();
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Console.Write(numbers[i] + " ");
+            }           
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 结构和枚举/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 结构和枚举
+{
+    class Program
+    {
+        public struct Person 
+        {
+            public string _Name;
+            public int _Age;
+            public Gender _Gender;
+        }
+        public enum Gender
+        {
+            男,
+            女
+        }
+        static void Main(string[] args)
+        {
+            Person zsperson;
+            zsperson._Name = "张三";
+            zsperson._Age = 20;
+            zsperson._Gender = Gender.男;
+            Console.WriteLine($"姓名：{zsperson._Name}，年龄：{zsperson._Age}，性别：{zsperson._Gender}"); // 姓名：张三，年龄：20，性别：男
+            Console.WriteLine("姓名：{0}，年龄：{1}，性别：{2}", zsperson._Name, zsperson._Age, zsperson._Gender); // 姓名：张三,年龄：20,性别：男);
+            Console.ReadKey();
+            Person lsperson;
+            lsperson._Name = "李四";
+            lsperson._Age = 18;
+            lsperson._Gender = Gender.女;
+            Console.WriteLine($"姓名：{lsperson._Name}，年龄：{lsperson._Age}，性别：{lsperson._Gender}"); // 姓名：李四，年龄：18，性别：女
+            Console.WriteLine("姓名：{0}，年龄：{1}，性别：{2}", lsperson._Name, lsperson._Age, lsperson._Gender); // 姓名：李四,年龄：18,性别：女);
+            Console.ReadKey();
+        }
+    }
+}
