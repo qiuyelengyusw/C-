5776461a5d9c5e557aa0d629680bc0709a39c546
Administrator <qiuyelengyu@qq.com>
2025-03-12 13:35:27 +0800

    复习

---
+++ 方法复习/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace 方法复习
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine(Add(1, 2));   
+            Console.ReadKey();  
+        }
+        public static int Add(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
--- 方法的练习/Program.cs
+++ 方法的练习/Program.cs
@@ -1,41 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace 方法的练习
+﻿namespace 方法的练习
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            //只允许输入yes或者no ，否则提示重新输入
-            Console.WriteLine("请输入yes或者no");
-            string stra = Console.ReadLine();   
-            IsYesOrNo(stra);
-            Console.WriteLine(stra);
-           // Console.ReadKey();
+            Console.WriteLine(Sum(1, 2));
+            Console.ReadKey();
         }
-        /// <summary>
-        /// 限定用户只能输入yes或者no
-        /// </summary>
-        /// <param name="strInput">需要判断的用户的输入内容</param>
-        /// <returns>返回限定的值</returns>
-        public static string IsYesOrNo(string strInput)
+        public static int Sum(int a, int b)
         {
-            while (true) 
-            {
-                if (strInput == "yes" || strInput == "no")
-                {
-                    return strInput;
-                }
-                else
-                {
-                    Console.WriteLine("输入错误！请重新输入");
-                    strInput = Console.ReadLine();
-                }
-            }
+            return a + b;
         }
     }
 }
